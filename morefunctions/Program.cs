c12a894ca7d05916530ca3bcac43c0e232f2fdde
jimin951u0 <108032556+jimin951u0@users.noreply.github.com>
2022-06-24 09:25:06 +1000

    inital transfer

---
+++ morefunctions/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+float functions(float pay) 
+{
+    float result = pay * 0.1f;
+    return result;
+
+
+}
+
+
+Console.WriteLine($"the result is {functions(10f)}");
+
+
+float calx(float amount) { 
+
+    float final = 
+
+
+}
