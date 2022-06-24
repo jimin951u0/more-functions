0e41318d424bd9efa1dfd6ce082cccb88187e5a3
jimin951u0 <108032556+jimin951u0@users.noreply.github.com>
2022-06-24 11:16:48 +1000

    calculation of strings

--- morefunctions/Program.cs
+++ morefunctions/Program.cs
@@ -14,9 +14,24 @@ float functions(float pay)
 Console.WriteLine($"the result is {functions(10f)}");
 
 
-float calx(float amount) { 
+float calx(float amount) {
 
-    float final = 
-
-
+    float finaltax = amount * 0.1f;
+    float final = finaltax + amount;
+    return final;
 }
+Console.WriteLine($"the final result is {calx(100)}");
+
+
+
+
+
+
+string str = "test";
+
+for (int i = 0; i < str.Length; i++)
+{
+    Console.Write(str[i] + " ");
+    
+}
+// Output: t e s t
